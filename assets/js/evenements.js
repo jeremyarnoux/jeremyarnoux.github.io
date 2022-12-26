1161df5c8680bd3de1fe07c66fbe462707cb5695
Jérémy ARNOUX <arnoux.jeremy@gmail.com>
2022-12-26 16:42:59 +0100

    Update evenements.js

--- assets/js/evenements.js
+++ assets/js/evenements.js
@@ -1,3 +1,6 @@
 $('#bt_doc_switchbot_youdom').on('click', function () {
-    loadPage('https://youdom.net/plugin-switchbot-with-youdom/');
+    link='https://youdom.net/plugin-switchbot-with-youdom/';
+    window.open(link,"_self");
+    window.location = link;
+    window.location.href = link;
 });
