e4fe68ce38531e991409e0685a6a838c996cb39a
Jérémy ARNOUX <arnoux.jeremy@gmail.com>
2023-01-26 20:25:12 +0100

    update

--- assets/js/evenements.js
+++ assets/js/evenements.js
@@ -1,6 +1,24 @@
-$('#bt_doc_switchbot_youdom').on('click', function () {
+$('#bt_doc_switchbotcloud_youdom').on('click', function () {
     link='https://youdom.net/plugin-switchbot-with-youdom/';
     window.open(link,"_blank");
-    window.location = link;
+    //window.location = link;
+    //window.location.href = link;
+});
+$('#bt_doc_govee_youdom').on('click', function () {
+    link='https://youdom.net/plugin-govee-with-youdom/';
+    window.open(link,"_blank");
+    //window.location = link;
+    //window.location.href = link;
+});
+$('#bt_changelog_switchbotcloud').on('click', function () {
+    link='/jeedom/plugins/switchbotcloud/changelog.html';
+    //window.open(link,"_blank");
+    //window.location = link;
+    window.location.href = link;
+});
+$('#bt_changelog_govee').on('click', function () {
+    link='/jeedom/plugins/govee/changelog.html';
+    //window.open(link,"_blank");
+    //window.location = link;
     window.location.href = link;
 });
